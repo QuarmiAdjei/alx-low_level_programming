593f3c2500a933f0c943f3250ce54a99f372c1b3
QuarmiAjei <dartison1@gmail.com>
2023-02-17 12:36:16 -0800

    program that prints the size of various types on the computer

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - begins here
+ *Return: Always 1 (rightful)
+ */
+int main(void)
+{write(STDOUT_FILEO,"and thst piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+return(1);
+}
