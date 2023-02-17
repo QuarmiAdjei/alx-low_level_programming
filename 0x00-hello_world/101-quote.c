2dfa1078852e40c2842f76c5a3245184a75aefb4
QuarmiAjei <dartison1@gmail.com>
2023-02-17 12:55:46 -0800

    program that prints the size of various types on the computer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-write(STDOUT_FILEO, "and thst piece of art is useful
-		\" - Dora Korpar, 2015-10-19\n", 59);
+fput "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
