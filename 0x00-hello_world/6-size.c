42b98cf752188e601e4f7773bdde1f781f3dad02
QuarmiAjei <dartison1@gmail.com>
2023-02-16 18:24:51 -0800

    updated c coding

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ ini main(void)
 	char d;
 	float f;
 
-	print("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(a));
-	printf("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(b));
-	printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-	print("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+	print("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	print("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
