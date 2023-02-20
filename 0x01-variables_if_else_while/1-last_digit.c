1e0aa325b25c993ba0fa94639445f5b96c4b6408
QuarmiAjei <dartison1@gmail.com>
2023-02-20 11:17:21 -0800

    random number veriables update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
-	if (last > $)
+	if (last > 5)
 		printf("last digit of %i is %i and is greater than $\n", n, last);
 	else if (last == 0)
 		printf("last digit of %i is %i and is 0\n", n last);
