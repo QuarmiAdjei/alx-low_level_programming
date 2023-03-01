02a35d9bdb1f63940aa3fd6c039d2a9511d28346
QuarmiAjei <dartison1@gmail.com>
2023-03-01 14:36:04 -0800

    task 2 update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,13 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char c;
 
-	while (i++ <= 9)
+	while (i <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
-		_putchar('/n');
+		{
+		_putchar(c);
+		}
+		_putchar('\n');
+		i++;
 	}
 }
