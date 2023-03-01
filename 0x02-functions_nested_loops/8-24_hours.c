44282b2d7e62e89116292623bedb3d3a16082cbe
QuarmiAjei <dartison1@gmail.com>
2023-03-01 15:26:43 -0800

    task 2 update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,9 @@
 void Jack_bauer(void)
 
 {
-	int a, b c, d;
+
+	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 		for  (b = 0; b <= 9; b++)
@@ -20,10 +22,10 @@ void Jack_bauer(void)
 					for (d = 0; d <= 9; d++)
 					{
 						_putchar(a + '0');
-						_putchar(a + '0');
+						_putchar(b + '0');
 						_putchar(58);
-						_putchar(a + '0');
-						_putchar(a + '0');
+						_putchar(c + '0');
+						_putchar(d + '0');
 						_putchar('\n');
 					}
 				}
