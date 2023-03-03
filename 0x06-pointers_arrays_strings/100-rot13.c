e2dc118bbcac16e6c632e375f363832ec5ab1763
QuarmiAjei <dartison1@gmail.com>
2023-03-02 21:06:52 -0800

    C - More pointers, arrays and strings C

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,14 @@
  */
 char *rot13(char *s)
 {
-	int j;
 	int i;
+	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j 0; j < 52; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
